0970e336b157d6acae47b6812752c19fd56bc616
simonphoenix96 <diazjustin1995@yahoo.de>
2021-03-14 15:15:56 +0100

    python subprocess call asynchronous

--- yt-livestream-grabber.py
+++ yt-livestream-grabber.py
@@ -19,6 +19,7 @@ soup = BeautifulSoup(page.text, 'html.parser')
 spec = soup.find_all("script")
 
 live = False
+streamlink_initiated = False
 
 # UNIQUE LIVE INDICATION THAT USER IS LIVE YT CHANGES THIS I THINK
 # {"text":"Live"}
@@ -35,7 +36,7 @@ if os.path.isfile(stream_folder + username + "_LIVE") == False:
 
         live_video_page = s.get('https://www.youtube.com/channel/' + channel + '/live')
         live_video_page_soup = BeautifulSoup(live_video_page.text, 'html.parser')
-        filename = live_video_page_soup.find_all("title")[0].text.replace(" - YouTube" , "")
+        filename = live_video_page_soup.find_all("title")[0].text.replace(" - YouTube" , "").replace("/", "_")
 
         potential_link = live_video_page_soup.find_all("link")
 
@@ -44,13 +45,18 @@ if os.path.isfile(stream_folder + username + "_LIVE") == False:
                 # add index where teaser html attribute starts to index where specific thread id starts
                 live_video_link = indice['href']
                 break
+        
 
-        command = 'echo "' + msg_streamer_live + '" && /usr/bin/streamlink -Q --force --hls-live-restart -o "' + stream_folder + filename + '.ts" "' + live_video_link + '" 720p'
-        # print(msg_streamer_live)
-        # command = '/usr/bin/streamlink -Q --force --hls-live-restart -o "' + stream_folder + filename + '.ts" "' + live_video_link + '" 720p'
-        subprocess.run(command, shell=True)
+
         f = open(stream_folder + username + "_LIVE", "w")
         f.close()
+        streamlink_initiated = True
+
+        command = '/usr/bin/streamlink --force --hls-live-restart -o "' + stream_folder + filename + '.ts" "' + live_video_link + '" 720p'
+
+        subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        
+        print(msg_streamer_live)
     else:
         print(username + " is not live!")
 
@@ -58,5 +64,5 @@ if live != True and os.path.isfile(stream_folder + username + "_LIVE"):
     print(username + " is not live so delete LIVE Marker file!")
     os.remove(stream_folder + username + "_LIVE")
 
-if live == True and os.path.isfile(stream_folder + username + "_LIVE"):
+if streamlink_initiated == False and live == True and os.path.isfile(stream_folder + username + "_LIVE"):
     print(username + "'s stream is currently being downloaded!")
